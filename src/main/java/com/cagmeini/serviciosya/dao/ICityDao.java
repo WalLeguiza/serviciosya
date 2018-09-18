b58c240b5d9becb884b5ee06fa16d7d2f04aea5c
WalLeguiza <wal.leguiza@gmail.com>
2018-09-18 17:50:23 -0300

    Added multiproject suport...

--- src/main/java/com/cagmeini/serviciosya/dao/ICityDao.java
+++
@@ -1,7 +0,0 @@
-package com.cagmeini.serviciosya.dao;
-
-import com.cagmeini.serviciosya.beans.entity.CityEntity;
-
-public interface ICityDao extends IDao <CityEntity, Integer> {
-
-}
--- src/main/java/com/cagmeini/serviciosya/dao/IContractDao.java
+++
@@ -1,7 +0,0 @@
-package com.cagmeini.serviciosya.dao;
-
-import com.cagmeini.serviciosya.beans.entity.ContractEntity;
-
-public interface IContractDao extends IDao<ContractEntity, Integer> {
-
-}
